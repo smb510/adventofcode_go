4d78851af9dc4bc5ad5653794c6168cbbd2ea568
Scottie Biddle <smb510@gmail.com>
2017-12-05 22:01:33 -0800

    bazel-ify all 6 existing projects

--- 2017/day4/day4.py
+++ 2017/day4/day4.py
@@ -1,16 +1,16 @@
+import os
+
 def main():
-	valid = 0
-	f = open("day4.txt", "r")
-	lines = list(f)
-	for line in lines:
-		words = [''.join(sorted(word)) for word in line.strip().split(" ")]
-		print words
-		if len(set(words)) == len(words):
-			valid += 1
-
-	print valid
-
-	f.close()
+    ws = os.path.dirname(__file__)
+    with open(os.path.join(ws, "day4.txt"), 'r') as f:
+        valid = 0
+        lines = list(f)
+        for line in lines:
+            words = [''.join(sorted(word)) for word in line.strip().split(" ")]
+            print words
+            if len(set(words)) == len(words):
+                valid += 1
+    print valid
 
 if __name__ == '__main__':
-	main()
+    main()
--- 2017/day5/day5.py
+++ 2017/day5/day5.py
@@ -1,24 +1,27 @@
-def main():
-	f = open("day5.txt", "r")
-	quotients = []
-	lines = [int(x.strip()) for x in list(f)]
-	pointer = 0
-	counter = 0
-	while pointer < len(lines):
-		pointer = visit(pointer, lines)
-		counter += 1
+import os
 
-	print counter
+def main():
+    ws = os.path.dirname(__file__)
+    with open(os.path.join(ws, "day5.txt"), 'r') as f:
+        quotients = []
+        lines = [int(x.strip()) for x in list(f)]
+        pointer = 0
+        counter = 0
+        while pointer < len(lines):
+            pointer = visit(pointer, lines)
+            counter += 1
+
+    print counter
 
 
 def visit(pointer, lines):
-	jmp = lines[pointer]
-	if jmp >= 3:
-		lines[pointer] -= 1
-	else:
-		lines[pointer] += 1
-	pointer += jmp
-	return pointer
+    jmp = lines[pointer]
+    if jmp >= 3:
+        lines[pointer] -= 1
+    else:
+        lines[pointer] += 1
+    pointer += jmp
+    return pointer
 
 if __name__ == '__main__':
-	main()
+    main()
