1056b5931f9c1c93d36beb473261ffe00c429b04
Scottie Biddle <smb510@gmail.com>
2017-12-18 20:04:45 -0800

    Zoom

--- 2017/day19/day19.py
+++ 2017/day19/day19.py
@@ -1,5 +1,9 @@
+import os
 def main():
-    print("Hi")
+    ws = os.path.dirname(__file__)
+    with open(os.path.join(ws, "day19.txt"), 'r') as f:
+        for line in f:
+            print(line.strip())
 
 if __name__ == '__main__':
     main()
