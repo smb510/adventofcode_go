a253dc582736891ef46e124a343b87419f23047e
Scottie Biddle <smb510@gmail.com>
2017-12-18 22:08:41 -0800

    refactored to a class

--- 2017/day19/day19.py
+++ 2017/day19/day19.py
@@ -1,62 +1,68 @@
 import os
 from enum import Enum
 class Direction(Enum):
+    DONE = -1
     UP = 0
     RIGHT = 1
     DOWN = 2
     LEFT = 3
 
-def main():
-    seen = ""
-    ws = os.path.dirname(__file__)
-    with open(os.path.join(ws, "day19.txt"), 'r') as f:
-        lines = [x.rstrip("\r\n") for x in list(f)]
-    grid = []
-    for line in lines:
-        grid.append([x for x in line])
-    row = 0
-    col = grid[0].index("|")
-    current_symbol = grid[row][col]
-    direction = Direction.DOWN
-    i = 1
+class Maze():
 
-    while True:
-        i += 1
-        # print(current_symbol, row, col)
-        if direction == Direction.DOWN:
-            row += 1
-        elif direction == Direction.UP:
-            row -= 1
-        elif direction == Direction.LEFT:
-            col -= 1
-        elif direction == Direction.RIGHT:
-            col += 1
-        current_symbol = grid[row][col]
-        if current_symbol == "+":
-            left = grid[row][col - 1] if direction != Direction.RIGHT else None
-            right = grid[row][col + 1] if direction != Direction.LEFT else None
-            up = grid[row - 1][col] if direction != Direction.DOWN else None
-            down = grid[row + 1][col] if direction != Direction.UP else None
-            directions = [up, right, down, left]
-            directions = [1 if x is not None and x != ' ' else 0 for x in directions]
-            try:
-                direction = Direction(directions.index(1))
-            except ValueError as e:
-                print(seen)
-                break
-        elif current_symbol.isalpha():
-            seen += current_symbol
-            left = grid[row][col - 1] if direction != Direction.RIGHT else None
-            right = grid[row][col + 1] if direction != Direction.LEFT else None
-            up = grid[row - 1][col] if direction != Direction.DOWN else None
-            down = grid[row + 1][col] if direction != Direction.UP else None
-            directions = [up, right, down, left]
-            directions = [1 if x is not None and x != ' ' else 0 for x in directions]
-            try:
-                direction = Direction(directions.index(1))
-            except ValueError as e:
-                print(seen, i)
-                break
+    def __init__(self):
+        ws = os.path.dirname(__file__)
+        with open(os.path.join(ws, "day19.txt"), 'r') as f:
+            lines = [x.rstrip("\r\n") for x in list(f)]
+        self.grid = []
+        for line in lines:
+            self.grid.append([x for x in line])
+        self.row = 0
+        self.col = self.grid[0].index("|")
+        self.update_symbol()
+        self.seen = ""
+        self.direction = Direction.DOWN
+        self.steps = 1
+
+    def move(self):
+        if self.direction == Direction.DOWN:
+            self.row += 1
+        elif self.direction == Direction.UP:
+            self.row -= 1
+        elif self.direction == Direction.LEFT:
+            self.col -= 1
+        elif self.direction == Direction.RIGHT:
+            self.col += 1
+        self.steps += 1
+
+    def update_symbol(self):
+        self.current_symbol = self.grid[self.row][self.col]
+        if self.current_symbol.isalpha():
+            self.seen += self.current_symbol
+
+    def update_direction(self):
+        left = self.grid[self.row][self.col - 1] if self.direction != Direction.RIGHT else None
+        right = self.grid[self.row][self.col + 1] if self.direction != Direction.LEFT else None
+        up = self.grid[self.row - 1][self.col] if self.direction != Direction.DOWN else None
+        down = self.grid[self.row + 1][self.col] if self.direction != Direction.UP else None
+        directions = [up, right, down, left]
+        directions = [1 if x is not None and x != ' ' else 0 for x in directions]
+        try:
+            self.direction = Direction(directions.index(1))
+        except ValueError as e:
+            self.direction = Direction.DONE
+
+    def zoom(self):
+        while self.direction != Direction.DONE:
+            self.move()
+            self.update_symbol()
+            if self.current_symbol == '+' or self.current_symbol.isalpha():
+                self.update_direction()
+            
+        print(self.seen, self.steps)
+
+def main():
+    maze = Maze()
+    maze.zoom()
     
 
 if __name__ == '__main__':
